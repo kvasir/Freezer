1a38461c4ba3994c6fd9e9abb4a169699d0dbdb4
Martin Sandström <maartiin_91@hotmail.com>
2015-11-22 13:15:48 +0100

    Ecmascriptsixify

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-'use strict'
-var app = require('app');
-var path = require('path');
-var BrowserWindow = require('browser-window');
+'use strict';
+const app = require('app');
+const path = require('path');
+const BrowserWindow = require('browser-window');
 
-var mainWindow = null;
+let mainWindow = null;
 
-app.on('window-all-closed', function() {
-	if (process.platform != 'darwin')
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
 		app.quit();
+	}
 });
 
 let ppapiFlashPath = null;
@@ -17,12 +18,12 @@ let ppapiFlashVersion = null;
 // On Windows, it might be /path/to/pepflashplayer.dll
 // On OS X, /path/to/PepperFlashPlayer.plugin
 // On Linux, /path/to/libpepflashplayer.so
-if(process.platform	== 'win32'){
+if (process.platform	=== 'win32') {
 	ppapiFlashPath = path.join(__dirname, 'pepper/win32/pepflashplayer.dll');
 	ppapiFlashVersion = '19.0.0.226';
-} else if (process.platform == 'linux') {
+} else if (process.platform === 'linux') {
 	ppapiFlashPath = path.join(__dirname, 'pepper/linux/libpepflashplayer.so');
-} else if (process.platform == 'darwin') {
+} else if (process.platform === 'darwin') {
 	ppapiFlashPath = path.join(__dirname, 'pepper/darwin/PepperFlashPlayer.plugin');
 	ppapiFlashVersion = '19.0.0.226';
 }
@@ -30,7 +31,7 @@ if(process.platform	== 'win32'){
 app.commandLine.appendSwitch('ppapi-flash-path', ppapiFlashPath);
 app.commandLine.appendSwitch('ppapi-flash-version', ppapiFlashVersion);
 
-app.on('ready', function() {
+app.on('ready', () => {
 	mainWindow = new BrowserWindow({
 		'width': 800,
 		'height': 600,
@@ -40,6 +41,6 @@ app.on('ready', function() {
 		}
 	});
 
-	mainWindow.openDevTools({ showDevTools: true });
+	mainWindow.openDevTools({showDevTools: true});
 	mainWindow.loadUrl('http://www.deezer.com');
 });
